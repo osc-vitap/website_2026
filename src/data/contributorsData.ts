21749874c456169bc9a13af84becd503b11c1429
Izhaan-Raza <izzupro3232@gmail.com>
2026-08-25 04:51:23 +0530

    Revert "chore(data): auto-sync GitHub organization contributors"
    This reverts commit b6853731ec3a84c35e269e407b9fe549ae6ba76f.

--- src/data/contributorsData.ts
+++ src/data/contributorsData.ts
@@ -25,11 +25,6 @@ export const contributorsData = [
     "html_url": "https://github.com/hardikxk"
   },
   {
-    "login": "iamsumedhsg",
-    "avatar_url": "https://avatars.githubusercontent.com/u/170885777?v=4",
-    "html_url": "https://github.com/iamsumedhsg"
-  },
-  {
     "login": "Izhaan-Raza",
     "avatar_url": "https://avatars.githubusercontent.com/u/66744194?v=4",
     "html_url": "https://github.com/Izhaan-Raza"
